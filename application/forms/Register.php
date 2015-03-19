6c983aff07684dabb122fcaf3d669d1b19d1335d
gehad <gehad_933@hotmail.com>
2015-03-19 14:00:01 +0200

    shklyat

--- application/forms/Register.php
+++ application/forms/Register.php
@@ -4,15 +4,16 @@ class Application_Form_Register extends Zend_Form {
 
     public function init() {
         $this->setMethod("post");
-
         $id = new Zend_Form_Element_Hidden("user_id");
-
+        
         $username = new Zend_Form_Element_Text("user_name");
+        $username->setAttrib('class', 'form-control');
         $username->setLabel("Username: ");
         $username->setRequired();
         $username->addFilter(new Zend_Filter_StripTags);
-
+        
         $gender = new Zend_Form_Element_Select("gender");
+        $gender->setAttrib('class', 'form-control');
         $gender->setRequired()
                 ->setLabel("Gender:")
                 ->addMultiOptions(array(
@@ -20,17 +21,15 @@ class Application_Form_Register extends Zend_Form {
                     'Male' => 'Male',
                     'Female' => 'Female'));
 
-
-
-
-
         $signature = new Zend_Form_Element_Text("signature");
+        $signature->setAttrib('class', 'form-control');
         $signature->setLabel("signature: ");
         $signature->setRequired();
         $signature->addFilter(new Zend_Filter_StripTags);
 
 
         $email = new Zend_Form_Element_Text("user_email");
+        $email->setAttrib('class', 'form-control');
         $email->setRequired()
                 ->setLabel("Email:")
                 ->addValidator(new Zend_Validate_EmailAddress())
@@ -40,6 +39,7 @@ class Application_Form_Register extends Zend_Form {
 
 
         $country = new Zend_Form_Element_Select("country");
+        $country->setAttrib('class', 'form-control');
         $country->setRequired()
                 ->setLabel("country:")
                 ->addMultiOptions(array(
@@ -48,6 +48,7 @@ class Application_Form_Register extends Zend_Form {
 
 
         $password = new Zend_Form_Element_Password("password");
+        $password->setAttrib('class', 'form-control');
         $password->setRequired()
                 ->setLabel("Password");
 
@@ -58,8 +59,10 @@ class Application_Form_Register extends Zend_Form {
 
 
         $submit = new Zend_Form_Element_Submit("submit");
+        $submit->setAttrib('class', 'btn btn-primary');
 
 
         $this->addElements(array($id, $username, $email, $gender, $password, $signature, $country, $photo, $submit));
     }
+
 }
